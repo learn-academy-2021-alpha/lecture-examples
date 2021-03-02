45ca7633f299ccfb4efb6c6456a76c3be97a6532
Mina Chuong <mina.chuong@gmail.com>
2021-03-02 12:12:37 -0800

    add ruby conditionals

---
+++ ruby/ruby-conditionals.rb
@@ -0,0 +1,30 @@
+# Ruby Conditionals
+
+# if/else/end
+# my_num = 9
+# if my_num == 10
+#   puts "Your number is 10!"
+# else
+#   puts "Your number is not 10."
+# end  
+
+# if/elsif/else/end
+
+# my_num = 4.3
+# if my_num == 10
+#   puts "Your number is 10!"
+# elsif my_num < 10
+#   puts "#{my_num} is less than 10."
+# elsif my_num > 10
+#   puts "#{my_num} is more than 10."
+# else
+#   puts "Something went wrong."
+# end
+
+# unless/else/end (if not)
+my_num = 10
+unless my_num > 20
+  puts "#{my_num} is not greater than 20"
+else
+  puts "#{my_num} is greater than 20"
+end
